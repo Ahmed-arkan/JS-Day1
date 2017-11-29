b253113fbf655ab12b2a025ffe731a8d00899cb7
admed-arkan <ahmedarkan@icloud.com>
2017-11-29 11:47:15 +0100

    JS1

---
+++ jsDay1.js
@@ -0,0 +1,8 @@
+var vienna = array["vienna","1","2","3","4"]
+var is= array["0","is","2","3","4"]
+var a = array["0","1","a","3","4"]
+var nice = array["0","1","2","nice","4"]
+var city = array["0","1","2","3","city"]
+
+
+console.log ( vienna[0] + is[1] + a [2]+ nice [3] + city [4])
